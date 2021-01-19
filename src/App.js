7dc11095893b9d86e1b6daede8f16d35a4241644
IvanSantana <iam.ivansantana@gmail.com>
2021-01-19 09:30:58 -0600

    Added alerta

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NuevaCuenta from "./components/auth/NuevaCuenta";
 import Proyectos from "./components/proyectos/Proyectos";
+import AlertaState from "./context/alertas/alertaState";
 import ProyectoState from "./context/proyectos/proyectoState";
 import TareaState from "./context/tareas/tareaState";
+ 
 
 
 function App() {
@@ -12,13 +14,15 @@ function App() {
     <>
       <ProyectoState>
         <TareaState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </Router>
+          <AlertaState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </Router>
+          </AlertaState>
         </TareaState>
       </ProyectoState> 
     </>
